887182e00f2648910d93fde261e9200113c08626
Umer Lachi <iamumerlachi@gmail.com>
2021-03-25 04:57:14 +0500

    Added Google Analytics and Facebook Pixel

--- components/HeadMeta.js
+++ components/HeadMeta.js
@@ -33,6 +33,47 @@ const HeadMeta = ({ name, title, desc, currentUrl }) => {
       <meta name='twitter:image' content={header_logo} />
 
       <link rel='canonical' href={currentUrl} />
+
+      <script
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=UA-34395334-1'
+      />
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-34395334-1');
+        `,
+        }}
+      />
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1672788266293461');
+          fbq('track', 'PageView');
+        `,
+        }}
+      />
+
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=1672788266293461&ev=PageView&noscript=1"
+          />`,
+        }}
+      />
     </Head>
   );
 };
--- components/WhatsappBtn.js
+++ components/WhatsappBtn.js
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 const WhatsappBtn = ({ ...props }) => {
   return (
     <a
-      class='cta-btn cta-whatsapp cta-btn-dark cta-btn-lg'
+      className='cta-btn cta-whatsapp cta-btn-dark cta-btn-lg'
       {...props}
       target='_self'
       href='https://wa.me/923335189005?text=I\%27m%20interested%20in%20your%20food'
