f770894ef2a8ffc39f812b2c318353e9b349c4fa
Umer Lachi <iamumerlachi@gmail.com>
2021-05-15 10:33:37 +0100

    bug fixes and improvements

--- components/DealItem.js
+++ components/DealItem.js
@@ -1,31 +1,29 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-const DealItem = ({ image, title, detailLink, children }) => {
-  return (
-    <Link href={detailLink}>
-      <a className='menu-item-card mb-5 w-100 rounded'>
-        <div
-          className='image'
-          style={{
-            height: '200px',
-            borderTopLeftRadius: '.75rem',
-            borderTopRightRadius: '.75rem',
-            overflow: 'hidden',
-          }}
-        >
-          <Image
-            style={{ objectFit: 'cover' }}
-            src={image}
-            alt={title}
-            width={400}
-            height={310}
-          />
-        </div>
-        <div className='info'>{children}</div>
-      </a>
-    </Link>
-  );
-};
+const DealItem = ({ image, title, detailLink, children }) => (
+  <Link href={detailLink}>
+    <a className='menu-item-card mb-5 w-100 rounded'>
+      <div
+        className='image'
+        style={{
+          height: '190px',
+          borderTopLeftRadius: '.75rem',
+          borderTopRightRadius: '.75rem',
+          overflow: 'hidden',
+        }}
+      >
+        <Image
+          src={image}
+          alt={title}
+          width={400}
+          height={300}
+          layout='responsive'
+        />
+      </div>
+      <div className='info'>{children}</div>
+    </a>
+  </Link>
+);
 
 export default DealItem;
